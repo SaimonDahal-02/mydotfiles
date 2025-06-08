797222313888eb25ec1c4b52a010a2dd8409e489
SaimonDahal-02 <saimondahal66@gmail.com>
2025-06-08 23:16:46 +0545

    update: nord theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
 			require("lualine").setup({
 				options = {
 					icons_enabled = true,
-					themes = "gruvbox",
+					themes = "nord",
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
